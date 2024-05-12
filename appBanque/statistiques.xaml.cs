3018263984d8a50f660d713c3a3dd9e88bd6ff39
SudoAldebaran <rayanmagniac@gmail.com>
2024-05-12 16:26:35 +0200

    Version finale

--- appBanque/statistiques.xaml.cs
+++ appBanque/statistiques.xaml.cs
@@ -25,6 +25,39 @@ namespace appBanque
         public statistiques(Banque b)
         {
             InitializeComponent();
+
+            this.laBanque = b;
+
+            double nbComptes = 0;
+            double soldeTotal = 0;
+            double soldeMax = double.MinValue;
+            double soldeMin = double.MaxValue;
+            double soldeNeg = 0;
+
+            foreach(Compte c in this.laBanque.mesComptes)
+            {
+                if (c.solde > soldeMax)
+                {
+                    soldeMax = c.solde;
+                }
+                if (c.solde < soldeMin)
+                {
+                    soldeMin = c.solde;
+                }
+                if (c.solde < 0)
+                {
+                    soldeNeg++;
+                }
+                soldeTotal = soldeTotal + c.solde;
+                nbComptes++;
+            }
+
+            this.nombreComptes.Text = nbComptes.ToString();
+            this.soldeMoyen.Text = (soldeTotal / nbComptes).ToString("0.00");
+            this.soldeMax.Text = soldeMax.ToString();
+            this.soldeMin.Text = soldeMin.ToString();
+            this.nbSoldesNegatifs.Text = soldeNeg.ToString();
+
         }
     }
 }
